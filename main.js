83bf76752938e76fb04370afa2dffb6da9f50422
Mauricio Herrera Ortega <ronalli56@gmail.com>
2023-03-15 16:59:46 -0300

    sweetalert2 agregado

--- main.js
+++ main.js
@@ -1,10 +1,17 @@
-console.log( "main.js loaded" );
 
-tsParticles
-  .loadJSON("tsparticles", "public/particles.json")
-  .then(container => {
-    console.log("callback - tsparticles config loaded");
-  })
-  .catch(error => {
-    console.error(error);
-  });
+console.log( "main.js loaded" );
+// tsParticles
+//   .loadJSON("tsparticles", "public/particles.json")
+//   .then(container => {
+//     console.log("callback - tsparticles config loaded");
+//   })
+//   .catch(error => {
+//     console.error(error);
+//   });
+
+// Swal.fire({
+//   title: 'Error!',
+//   text: 'Do you want to continue',
+//   icon: 'error',
+//   confirmButtonText: 'Cool'
+// })
